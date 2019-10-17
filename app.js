8f3a3c9040d6d9ca0cc81c0dc1a45ed69d934cb4
Ramon Sanchez <songwright66@gmail.com>
2019-10-17 01:49:22 -0700

    Added intern function to app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const inquirer = require("inquirer");
+const confirm = require('inquirer-confirm');
 const Manager = require("../Develop/lib/Manager");
-const makeHTML = require("../Develop/lib/makeHTML");
+const Intern = require("../Develop/lib/Intern");
+const makeMgrHTML = require("./lib/makeMgrHTML");
+const makeIntHTML = require("./lib/makeIntHTML");
 
 const mgrQuestions = [
   {
@@ -21,7 +24,7 @@ const mgrQuestions = [
   {
     type: "input",
     message: "Enter the manager's office number.",
-    name: "mgrOffice"
+    name: "office"
   }
 ]
 
@@ -34,12 +37,35 @@ const teamQuestions = [
   }
 ]
 
+const intQuestions = [
+  {
+    type: "input",
+    message: "what is the intern's name?",
+    name: "intName"
+  },
+  {
+    type: "input",
+    message: "Enter the intern's id.",
+    name: "intID"
+  },
+  {
+    type: "input",
+    message: "Enter the intern's e-mail address.",
+    name: "intEmail"
+  },
+  {
+    type: "input",
+    message: "Enter the intern's school.",
+    name: "school"
+  }
+]
+
 function team() {
   inquirer
     .prompt(teamQuestions)
     .then(team => {
       if (team.member === "Intern") {
-        console.log("Intern")
+        intern();
       } else {
         console.log("Engineer")
       }
@@ -49,12 +75,31 @@ function team() {
     })
 }
 
+function intern() {
+  inquirer
+    .prompt(intQuestions)
+    .then(intern => {
+      let newIntern = new Intern(intern.intName, intern.intID, intern.intEmail, intern.school);
+      let html = makeIntHTML.makeIntHTML(newIntern);
+      console.log(html);
+      confirm("Continue adding team members?")
+        .then(function confirmed() {
+          team();
+        }, function cancelled() {
+          console.log("Team completed.");
+        });
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+}
+
 inquirer
   .prompt(mgrQuestions)
   .then(manager => {
-    let manager1 = new Manager(manager.mgrName, manager.mgrID,
-      manager.mgrEmail, manager.mgrOffice);
-      let html = makeHTML.makeHTML(manager1);
+    let newManager = new Manager(manager.mgrName, manager.mgrID,
+      manager.mgrEmail, manager.office);
+      let html = makeMgrHTML.makeMgrHTML(newManager);
       console.log(html);
       team();
   })
